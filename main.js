e75406a173d9d82fab635b81d5e252e6a3d65a85
Rites1 <103243296+Rites1@users.noreply.github.com>
2023-06-13 20:56:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
-function preload(){
+nose_x = 0;
+nose_y = 0;
 
+function preload(){
+    moustache = loadImage('https://i.postimg.cc/9MBx7HX8/m.png');
 }
 
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    posenet = ml5.poseNet(video, modelloaded);
+    posenet.on('pose', gotPoses);
+}
+
+function modelloaded(){
+    console.log("posenet is initalized");
 }
 
 function draw(){
-
+    image(video, 0, 0, 300, 300);
+    image(moustache, nose_x, nose_y, 80, 35);
 }
 
 function take_snapshot(){
     save('image.png');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("nose x = "+ results[0].pose.nose.x);
+        console.log("nose y = "+ results[0].pose.nose.y);
+        nose_x = results[0].pose.nose.x - 40;
+        nose_y = results[0].pose.nose.y;
+    }
 }
